a4f6a8d4341c8c3b2647077ca186c2ec786d4f84
Leonardo Petruncko <leonardopetruncko20@gmail.com>
2022-12-11 19:10:05 -0300

    refatorando código

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ const itens = JSON.parse(localStorage.getItem
     ("itens")) || [];
 
 itens.forEach((elemento) => {
-    console.log(elemento.nome, elemento.quantidade)
+    criaElemento(elemento)
 });
 
 form.addEventListener("submit", (evento) => {
@@ -14,34 +14,33 @@ form.addEventListener("submit", (evento) => {
     const nome = evento.target.elements['nome'];
     const quantidade = evento.target.elements['quantidade'];
 
-    criaElemento(nome.value, quantidade.value)
+    const itemAtual = {
+        "nome": nome.value,
+        "quantidade": quantidade.value
+    }
+
+    criaElemento(itemAtual)
+
+    itens.push(itemAtual)
+
+    localStorage.setItem("itens", JSON.stringify(itens))
 
     nome.value = "";
     quantidade.value = "";
 
 });
 
-function criaElemento(nome, quantidade) {
+function criaElemento(item) {
 
-    const novoItem = document.createElement('li');
-    novoItem.classList.add("item");
-    
-    const numeroItem = document.createElement('strong');
-    numeroItem.innerHTML = quantidade;
-    
-    novoItem.appendChild(numeroItem)
-    novoItem.innerHTML += nome
-    
-    lista.appendChild(novoItem)
-    
-    const itemAtual = {
-        "nome": nome,
-        "quantidade": quantidade
-    }
-    
-    itens.push(itemAtual)
-    
-    localStorage.setItem("item", JSON.stringify(itens))
-    
-    }
-    
+const novoItem = document.createElement('li');
+novoItem.classList.add("item");
+
+const numeroItem = document.createElement('strong');
+numeroItem.innerHTML = item.quantidade;
+
+novoItem.appendChild(numeroItem)
+novoItem.innerHTML += item.nome
+
+lista.appendChild(novoItem)
+
+}
