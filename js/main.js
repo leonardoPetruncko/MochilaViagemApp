76d1819583ccb2ff21422ba58dd3007155390904
Leonardo Petruncko <leonardopetruncko20@gmail.com>
2022-12-11 19:09:10 -0300

    itensSalvos Array

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 const form = document.getElementById("novoItem");
 const lista = document.getElementById("lista");
 
-const itens = [];
+const itens = JSON.parse(localStorage.getItem
+    ("itens")) || [];
+
+itens.forEach((elemento) => {
+    console.log(elemento.nome, elemento.quantidade)
+});
 
 form.addEventListener("submit", (evento) => {
     evento.preventDefault();
